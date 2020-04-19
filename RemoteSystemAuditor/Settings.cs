a3c338259c111a938b4fa087af1bed75c4e39896
Plus1XP <evldev@protonmail.ch>
2020-04-19 13:13:02 +0100

    Fix missing references to previously uploaded class files

---
+++ RemoteSystemAuditor/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace RemoteSystemAuditor
+{
+    public class Settings
+    {
+        public string host = "smtp.example.com";
+        public int port = 587;
+        public string emailAddress = "sender@example.com";
+        public string userName = "username";
+        public string password = "password";
+        public string destinationEmail = "recipient@example.com";
+    }
+}
