0f1db2d844da95c4ae71aaef18fea28aee0231a7
Plus1XP <evldev@protonmail.ch>
2020-04-19 13:42:02 +0100

    Add Program class logic (UI)

--- RemoteSystemAuditor/Program.cs
+++ RemoteSystemAuditor/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace RemoteSystemAuditor
 {
     class Program
     {
+        // Rename the Executable file once compiled, with a meaningful name to keep track of the audits.
+        public static string ExecutableName = $"{Path.GetFileName(Assembly.GetEntryAssembly().Location).Before(".exe")}";
         static void Main(string[] args)
         {
+            Audit audit = new Audit();
+
+            Console.WriteLine("Please Wait...");
+            string results = audit.GetSystemInfo(ExecutableName);
+            Console.WriteLine(results);
+            SendMail.NewMessage($"System Audit {ExecutableName}", results);
+            Console.WriteLine("Press Enter to exit");
+            Console.ReadLine();
         }
     }
 }
